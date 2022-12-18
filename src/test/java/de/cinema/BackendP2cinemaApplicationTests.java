2f769a159544c5dac0a2c36c01d09958814fde76
marcbudd <marcbuddemeier@gmail.com>
2022-12-18 20:11:19 +0100

    Initial commit

---
+++ src/test/java/de/cinema/BackendP2cinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendP2cinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

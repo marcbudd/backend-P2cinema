2eca445b7a1b9c09d60d2b2b1bfaf34241614295
marcbudd <marcbuddemeier@gmail.com>
2022-12-21 14:21:05 +0100

    Add database url and credentials

---
+++ src/test/java/de/backendP2cinema/BackendP2cinemaApplicationTests.java
@@ -0,0 +1,13 @@
+package de.backendP2cinema;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendP2cinemaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
